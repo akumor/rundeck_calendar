454cc12e5e39991ff924af99790b4f7db9314f46
Jan Kumor <kumor@nodalexchange.com>
2016-09-05 11:24:24 -0400

    Added some code for managing the Docker container that will run Rundeck in order to test the API

--- tests/TestRundeckCalendar.py
+++ tests/TestRundeckCalendar.py
@@ -4,7 +4,9 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 import rundeck_calendar
 import unittest
-
+from docker import Client
+import json
+import requests
 
 class TestRundeckCalendar(unittest.TestCase):
     """
@@ -15,7 +17,18 @@ class TestRundeckCalendar(unittest.TestCase):
         """
         Prepare to run test.
         """
-        pass
+        global docker_client
+        global container
+        docker_client = Client(base_url='unix://var/run/docker.sock')
+        for line in docker_client.pull('jordan/rundeck', stream=True):
+            print(line.decode('utf-8'))
+        container = docker_client.create_container('jordan/rundeck',
+                                                   detach=True,
+                                                   ports=['4440'],
+                                                   environment=['SERVER_URL=http://localhost:4440'])
+        docker_client.start(container=container.get('Id'))
+        resp = requests
+        # TODO use requests library with the admin/admin account to generate a cookie that can be used for API requests
 
     def tearDown(self):
         """
